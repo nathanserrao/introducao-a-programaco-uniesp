b03a08bb7f40727219442892a3a336fe699c7e74
nathanserrao <101150812+nathanserrao@users.noreply.github.com>
2022-05-14 22:31:28 -0300

    Add files via upload

---
+++ Ted_12_13/ted.py
@@ -0,0 +1,17 @@
+from time import sleep
+glossário = {"Python":" Uma linguagem de programação", "VScode":" uma IDE", "Jared Leto":" um ator de Hollywood" , "Morbius":"Um filme de Jared Leto"
+, "Canoa":"Um meio de transporte aquático"
+
+
+
+}
+
+
+for i in glossário:
+    sleep(1)
+    print("\n---------------------")
+    print(f"{i} é {glossário[i]}")
+
+
+
+
