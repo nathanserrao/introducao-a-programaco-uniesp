1b6f7d98758602af4f5e91cfc0347eb2bb04a2d4
nathanserrao <101150812+nathanserrao@users.noreply.github.com>
2022-05-15 10:14:57 -0300

    Update ted.py

--- Ted_12_13/ted.py
+++ Ted_12_13/ted.py
@@ -1,6 +1,7 @@
 from time import sleep
-glossário = {"Python":" Uma linguagem de programação", "VScode":" uma IDE", "Jared Leto":" um ator de Hollywood" , "Morbius":"Um filme de Jared Leto"
-, "Canoa":"Um meio de transporte aquático"
+glossário = {"Python":" Uma linguagem de programação", "VScode":" uma IDE", "Pycharm":" é uma IDE focada em Python" , "Web scraping":
+" é um processo de coleta de dados estruturados da web de maneira automatizada"
+, "Google Colab":"Google Colab é um serviço de armazenamento em nuvem de notebooks voltados à criação e execução de códigos em Python, diretamente em um navegador"
 
 
 
